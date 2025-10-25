8500f9bc09a594ddf5fd172dbc310a19d529c37f
stefanskoricdev <stefan@crocoder.dev>
2025-10-25 16:47:25 +0200

    Handle publish and add writer function for subscribed connections

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"sync"
 
@@ -13,7 +15,7 @@ type Message interface{}
 type Subscriber struct {
 	id        string
 	conn      *websocket.Conn
-	send      chan Message
+	channel   chan Message
 	topicName string
 }
 
@@ -34,7 +36,7 @@ func NewPubSub() *PubSub {
 	}
 }
 
-func (pubSub *PubSub) getTopic(topicName string) *Topic {
+func (pubSub *PubSub) getOrCreateTopic(topicName string) *Topic {
 	pubSub.mu.Lock() // Acquire WRITE lock for PubSub service
 	defer pubSub.mu.Unlock()
 
@@ -44,17 +46,21 @@ func (pubSub *PubSub) getTopic(topicName string) *Topic {
 		return topic
 	}
 
-	return &Topic{
+	newTopic := &Topic{
 		Name:        topicName,
 		subscribers: make(map[string]*Subscriber),
 		// The RWMutex is initialized with 0 value
 	}
+
+	pubSub.topics[topicName] = newTopic
+
+	return newTopic
 }
 
 func (topic *Topic) Unsubscribe(subscriberID string) {
 	subscriber := topic.subscribers[subscriberID]
 
-	close(subscriber.send)
+	close(subscriber.channel)
 	delete(topic.subscribers, subscriberID)
 }
 
@@ -65,8 +71,10 @@ var upgrader = websocket.Upgrader{
 }
 
 func (pubSub *PubSub) Subscribe(w http.ResponseWriter, r *http.Request) error {
-	topic := pubSub.getTopic(r.PathValue("topic"))
+	topic := pubSub.getOrCreateTopic(r.PathValue("topic"))
 	subscriberID := r.URL.Query().Get("id")
+
+	log.Printf("Subscribed! Topic: %s, ID: %s", topic.Name, subscriberID)
 
 	topic.mu.Lock() // Acquire WRITE lock for topic subscribers
 	defer topic.mu.Unlock()
@@ -78,22 +86,74 @@ func (pubSub *PubSub) Subscribe(w http.ResponseWriter, r *http.Request) error {
 		topic.Unsubscribe(subscriberID)
 	}
 
-	// Create ws connection
+	// Upgrade to WS connection
 	conn, err := upgrader.Upgrade(w, r, nil)
 
 	if err != nil {
 		return fmt.Errorf("could not upgrade to websocket connection")
 	}
-	// Create channel
 
-	topic.subscribers[subscriberID] = &Subscriber{
+	// Create subscriber
+	sub := &Subscriber{
 		id:        subscriberID,
 		conn:      conn,
-		send:      make(chan Message, 64),
+		channel:   make(chan Message, 64),
 		topicName: topic.Name,
 	}
 
+	topic.subscribers[subscriberID] = sub
+
+	// Start dedicated listener go routine for this subscriber.
+	// This go routine handles all published messages to its topic
+	// and sends them to client
+	go sub.writer()
+
 	return nil
+}
+
+func (wss *Subscriber) writer() {
+	for message := range wss.channel {
+		log.Println("Received message!")
+		log.Println(message)
+
+		// TO-DO send message to client through WS connection
+	}
+}
+
+func (ps *PubSub) Publish(topicName string, message Message) error {
+	ps.mu.RLock()
+	topic, exists := ps.topics[topicName]
+	defer ps.mu.RUnlock()
+
+	if !exists {
+		return fmt.Errorf("topic '%s' not found", topicName)
+	}
+
+	// Create dedicated goroutine for
+	// each subscriber to fan out message
+	for _, sub := range topic.subscribers {
+		go func(s *Subscriber, m Message) {
+			s.channel <- m
+			log.Printf("Message sent: %s", m)
+		}(sub, message)
+	}
+
+	return nil
+}
+
+var service = NewPubSub()
+
+func handlePublish(w http.ResponseWriter, r *http.Request) {
+	topicName := r.PathValue("topic")
+
+	// Read the message body from the HTTP request.
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	service.Publish(topicName, string(body))
 }
 
 func handleWSSubscribe(w http.ResponseWriter, r *http.Request) {
@@ -102,16 +162,16 @@ func handleWSSubscribe(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var service = NewPubSub()
-
 func main() {
-	fmt.Println("Websocket server started at :8080")
+	log.Println("Websocket server started at :8080")
 
-	http.HandleFunc("/ws/subscribe/{topic}", handleWSSubscribe)
+	http.HandleFunc("POST /pub/{topic}", handlePublish)
+
+	http.HandleFunc("GET /ws/sub/{topic}", handleWSSubscribe)
 
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		log.Println("Error starting server:", err)
 	}
 }
